aed8b9ff0e850830c61ecf8518cb662fdfe42781
Edmund Mai <edmundm@crowdtap.com>
2015-05-02 15:37:10 -0400

    adds customer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502192806) do
+ActiveRecord::Schema.define(version: 20150502193400) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,13 @@ ActiveRecord::Schema.define(version: 20150502192806) do
 
   add_index "clients", ["email"], name: "index_clients_on_email", unique: true
   add_index "clients", ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+
+  create_table "customers", force: :cascade do |t|
+    t.integer  "client_id"
+    t.string   "name"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end

81f624cff2e1a7ec9ce138797476c3327d2beb36
Edmund Mai <edmundm@crowdtap.com>
2015-05-03 01:03:35 -0400

    fix sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < Devise::RegistrationsController
+class SessionsController < Devise::SessionsController
   skip_before_filter :verify_authenticity_token
   respond_to :json
 end

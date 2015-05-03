fac334879ec0df5c0c2a76557a3948726819a8c3
Edmund Mai <edmundm@crowdtap.com>
2015-05-02 22:10:09 -0400

    allow html

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -1,10 +1,14 @@
 class QueuesController < ApplicationController
-  before_action :authenticate_client!
+  # before_action :authenticate_client!
 
   def show
     @queue = current_client.customers_in_line
     @customer = Customer.new
 
     @average_wait_time = Customer.average_wait_time
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end

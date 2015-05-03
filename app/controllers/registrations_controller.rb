4cc66732521c839c5d7613e31ae0da12626aeb79
Edmund Mai <edmundm@crowdtap.com>
2015-05-03 01:07:55 -0400

    add form fields

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_filter :verify_authenticity_token
   respond_to :json
+  p
+  before_filter :configure_permitted_parameters
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:industry, :company_name)
+  end
 end

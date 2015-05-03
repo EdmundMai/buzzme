4de9064432c716b0d7a857741d777818272ffca6
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-05-03 10:30:07 -0400

    test

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -60,6 +60,7 @@ class CustomersController < ApplicationController
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     auth_token = ENV['TWILIO_AUTH_TOKEN']
 
+
     begin
       client = Twilio::REST::Client.new account_sid, auth_token
       client.messages.create(
